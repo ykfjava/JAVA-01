828ef339298012bbc4a783b2c2925836ac76a870
ykfjava <13991022971@sina.cn>
2021-02-28 01:11:34 +0800

    Signed-off-by: ykfjava <13991022971@sina.cn> ykf5525182..
    Signed-off-by: ykfjava <13991022971@sina.cn>

---
+++ Week_06/mall_javaup.sql
@@ -0,0 +1,143 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 虚拟机
+ Source Server Type    : MySQL
+ Source Server Version : 50724
+ Source Host           : 192.168.68.131:3306
+ Source Schema         : mall_javaup
+
+ Target Server Type    : MySQL
+ Target Server Version : 50724
+ File Encoding         : 65001
+
+ Date: 28/02/2021 01:01:52
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for oms_order
+-- ----------------------------
+DROP TABLE IF EXISTS `oms_order`;
+CREATE TABLE `oms_order`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `order_code` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '订单code',
+  `create_time` timestamp(0) NULL DEFAULT NULL COMMENT '提交时间',
+  `member_username` varchar(512) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户帐号',
+  `total_amount` decimal(10, 2) NULL DEFAULT NULL COMMENT '订单总金额',
+  `pay_amount` decimal(10, 2) NULL DEFAULT 0.00 COMMENT '应付金额（实际支付金额）',
+  `freight_amount` decimal(10, 2) NULL DEFAULT NULL COMMENT '运费金额',
+  `promotion_amount` decimal(10, 2) NULL DEFAULT NULL COMMENT '促销优化金额（促销价、满减、阶梯价）',
+  `integration_amount` decimal(10, 2) NULL DEFAULT NULL COMMENT '积分抵扣金额',
+  `coupon_amount` decimal(10, 2) NULL DEFAULT NULL COMMENT '优惠券抵扣金额',
+  `discount_amount` decimal(10, 2) NULL DEFAULT NULL COMMENT '管理员后台调整订单使用的折扣金额',
+  `pay_type` smallint(1) NULL DEFAULT NULL COMMENT '支付方式：0->未支付；1->支付宝；2->微信',
+  `source_type` smallint(1) NULL DEFAULT NULL COMMENT '订单来源：0->PC订单；1->app订单',
+  `status` smallint(1) NULL DEFAULT NULL COMMENT '订单状态：0->待付款；1->待发货；2->已发货；3->已完成；4->已关闭；5->无效订单',
+  `order_type` smallint(1) NULL DEFAULT NULL COMMENT '订单类型：0->正常订单；1->秒杀订单',
+  `confirm_status` smallint(1) NULL DEFAULT NULL COMMENT '确认收货状态：0->未确认；1->已确认',
+  `delete_status` smallint(1) NOT NULL DEFAULT 0 COMMENT '删除状态：0->未删除；1->已删除',
+  `use_integration` int(11) NULL DEFAULT NULL COMMENT '下单时使用的积分',
+  `payment_time` timestamp(0) NULL DEFAULT NULL COMMENT '支付时间',
+  `delivery_time` timestamp(0) NULL DEFAULT NULL COMMENT '发货时间',
+  `receive_time` timestamp(0) NULL DEFAULT NULL COMMENT '确认收货时间',
+  `comment_time` timestamp(0) NULL DEFAULT NULL COMMENT '评价时间',
+  `modify_time` timestamp(0) NULL DEFAULT NULL COMMENT '修改时间',
+  `store_id` bigint(20) NULL DEFAULT NULL,
+  `tax_type` smallint(6) NULL DEFAULT 1 COMMENT '是否开发票 1=不发票 2=个人发票 3=公司发票',
+  PRIMARY KEY (`id`, `order_code`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1000000 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for oms_order_attr
+-- ----------------------------
+DROP TABLE IF EXISTS `oms_order_attr`;
+CREATE TABLE `oms_order_attr`  (
+  `order_id` bigint(20) NOT NULL,
+  `goods_id` bigint(20) NULL DEFAULT NULL COMMENT '商品id',
+  `delivery_company` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '物流公司(配送方式)',
+  `delivery_sn` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '物流单号',
+  `auto_confirm_day` int(11) NULL DEFAULT NULL COMMENT '自动确认时间（天）',
+  `goods_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '品名',
+  `integration` int(11) NULL DEFAULT NULL COMMENT '可以获得的积分',
+  `growth` int(11) NULL DEFAULT NULL COMMENT '可以活动的成长值',
+  `bill_content` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '发票内容',
+  `bill_receiver_phone` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '收票人电话',
+  `bill_receiver_email` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '收票人邮箱',
+  `receiver_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '收货人姓名',
+  `receiver_phone` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '收货人电话',
+  `receiver_post_code` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '收货人邮编',
+  `receiver_province` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '省份/直辖市',
+  `receiver_city` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '城市',
+  `receiver_region` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '区',
+  `receiver_detail_address` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '详细地址',
+  `note` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '订单备注',
+  UNIQUE INDEX `uniq_order_id`(`order_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for pms_goods
+-- ----------------------------
+DROP TABLE IF EXISTS `pms_goods`;
+CREATE TABLE `pms_goods`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `pic` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '图片路径',
+  `name` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `price` decimal(10, 2) NULL DEFAULT NULL COMMENT '价格',
+  `product_sn` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '货号',
+  `sub_title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '副标题',
+  `description` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '商品描述',
+  `original_price` decimal(10, 2) NULL DEFAULT NULL COMMENT '市场价',
+  `stock` int(11) NULL DEFAULT NULL COMMENT '库存',
+  `low_stock` int(11) NULL DEFAULT NULL COMMENT '库存预警值',
+  `unit` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '单位',
+  `weight` decimal(10, 2) NULL DEFAULT NULL COMMENT '商品重量，默认为克',
+  `delete_status` smallint(1) NULL DEFAULT 0 COMMENT '删除状态：0->未删除；1->已删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for ums_user
+-- ----------------------------
+DROP TABLE IF EXISTS `ums_user`;
+CREATE TABLE `ums_user`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `password` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `store_id` bigint(20) NULL DEFAULT NULL,
+  `delete_status` smallint(1) NULL DEFAULT 0 COMMENT '删除状态：0->未删除；1->已删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1000 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Procedure structure for loopAddOrder
+-- ----------------------------
+DROP PROCEDURE IF EXISTS `loopAddOrder`;
+delimiter ;;
+CREATE DEFINER=`root`@`%` PROCEDURE `loopAddOrder`()
+begin
+
+DECLARE i int; 
+DECLARE order_id int;
+DECLARE time bigint;
+set i = 1;
+set time = unix_timestamp(now());
+lp1:LOOP
+set i = i+1;
+
+INSERT INTO `mall_javaup`.`oms_order`( `order_code`, `create_time`, `member_username`, `total_amount`, `pay_amount`, `freight_amount`, `promotion_amount`, `integration_amount`, `coupon_amount`, `discount_amount`, `pay_type`, `source_type`, `status`, `order_type`, `confirm_status`, `delete_status`, `use_integration`, `payment_time`, `delivery_time`, `receive_time`, `comment_time`, `modify_time`, `store_id`, `tax_type`) VALUES ( CONCAT('jkorder',i), NOW(), 'admin', 23.66, 23.66, 23.66, 23.66, 23.66, 23.66, 23.66, 1, 1, 3, 0, 1, 0, 0, NOW(), NOW(), NOW(), NOW(), NOW(), 1, 1);
+
+
+INSERT INTO `mall_javaup`.`oms_order_attr`(order_id , `goods_id`, `delivery_company`, `delivery_sn`, `auto_confirm_day`, `goods_name`, `integration`, `growth`, `bill_content`, `bill_receiver_phone`, `bill_receiver_email`, `receiver_name`, `receiver_phone`, `receiver_post_code`, `receiver_province`, `receiver_city`, `receiver_region`, `receiver_detail_address`, `note`) VALUES (LAST_INSERT_ID(), 1, 'ZTO', '714225021021', 14, '品名', 1, 1, '发票内容', '收票人电话', '收票人邮箱', '收货人姓名', '收货人电话', '收货人邮编', '省份/直辖市', '城市', '区', '详细地址', '订单备注');
+
+if i>=1000000 then
+leave lp1;
+end if;
+end loop;
+END
+;;
+delimiter ;
+
+SET FOREIGN_KEY_CHECKS = 1;
